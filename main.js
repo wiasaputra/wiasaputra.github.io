37a64bab1bebacf0b785d7dbbf5c16f2b09e8282
wiasaputra <70959991+wiasaputra@users.noreply.github.com>
2022-11-11 17:29:48 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,13 +6,27 @@ const listItems =[
         name: "Carved buffalo skull Indian huge skull",
         price: 5500000,
         img: './assets/images/il_794xN.3228679722_oz0l.jpg',
-        car:['car1','car2','car3']
+        car:[
+            'https://i.etsystatic.com/27320701/r/il/db904d/3228663256/il_794xN.3228663256_m1as.jpg',
+            'https://i.etsystatic.com/27320701/r/il/dd59a9/3228663416/il_794xN.3228663416_rvfe.jpg',
+            'https://i.etsystatic.com/27320701/r/il/ada1ab/3666810339/il_794xN.3666810339_4xk3.jpg',
+            'https://i.etsystatic.com/27320701/r/il/6ff98d/3619201080/il_794xN.3619201080_i0u5.jpg',
+            'https://i.etsystatic.com/27320701/r/il/b53cda/3619201152/il_794xN.3619201152_ozfq.jpg',
+            'https://i.etsystatic.com/27320701/r/il/086d7c/3619203144/il_794xN.3619203144_phz7.jpg'
+        ]
     },
     {id:2,
         name: "carved buffalo skull carving Lotus padma",
         price: 5500000,
         img: "./assets/images/il_794xN.3725923346_106v.jpg",
-        car:['car10','car20','car30']
+        car:[
+            'https://i.etsystatic.com/27320701/r/il/a25970/3725923688/il_794xN.3725923688_pyba.jpg',
+            'https://i.etsystatic.com/27320701/r/il/57219a/3725923914/il_794xN.3725923914_td7r.jpg',
+            'https://i.etsystatic.com/27320701/r/il/3dbd3f/3773509319/il_794xN.3773509319_qkmz.jpg',
+            'https://i.etsystatic.com/27320701/r/il/53bb6c/3725923792/il_794xN.3725923792_ri2z.jpg',
+            
+            
+        ]
     },
     {id:3,
         name: "carve skull buffalo Mandala lotus with carved long horn",
@@ -30,6 +44,12 @@ const listItems =[
         name: "Carved buffalo skull Indian natif america",
         price: 5600000,
         img: './assets/images/il_794xN.3721666122_llft.jpg',
+        car:['car111','car222','car333']
+    },
+    {id:6,
+        name: "buffalo skull carving floral grey",
+        price: 5600000,
+        img: './assets/images/il_794xN.3759067025_3qgx.jpg',
         car:['car111','car222','car333']
     },
     
@@ -53,31 +73,33 @@ shop.innerHTML=listItems.map((item, index)=>{
                 <h5 class="card-text">${rupiah(item.price)}</h5>
                 </div>
                 <button type="button" class="btn btn-primary btnDetail" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                Detail product
+                <i class="bi bi-window-desktop"></i> Detail product
                 </button>
             </div>  
       </div>
      `
 }).join("")
 
+const whatAppChat =()=>{
+window.open("https://wa.me/6287861345373?text=%2AHallo%20Wiasa%2C%2A%0AI%20am%20interested%20in%20the%20product%0ACan%20I%20order%20it")
+}
 
 
 const btnDetail = document.querySelectorAll('.btnDetail')
-console.log(btnDetail)
 let choosenProduct = listItems[0]
 btnDetail.forEach((data, index)=> {
-    data.addEventListener('click', ()=>{
-        console.log('click terjadi', index)
+    data.addEventListener('click', ()=>{ 
         const modalContent =document.querySelector('.modal-content')
-        console.log(modalContent)
         modalContent.innerHTML =
        `
        <div class="modal-header">
-       <h1 class="modal-title fs-5" id="exampleModalLabel">${listItems[index].name}</h1>
+       <h1 class="modal-title fs-5 text-uppercase text-center" id="exampleModalLabel">${listItems[index].name}</h1>
        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
      </div>
-     <div class="modal-body">
-       Isi apa disini
+     <div class="modal-body text-center">
+     ${listItems[index].car.map(e=>{return `<img class="img-fluid mb-3" src="${e}"/>`}).join("")}
+       <div>For more detail information send Whatapp</div>
+       <button type="button" class="btn btn-outline-success" onclick="whatAppChat()">Contact Here</button>
      </div>
      <div class="modal-footer">
        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -86,3 +108,4 @@ btnDetail.forEach((data, index)=> {
        `
     })
 });
+
